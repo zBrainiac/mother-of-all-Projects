a25079be20b06864f5bc3897a2505994b73a6b83
sfc-gh-mdaeppen <marcel.daeppen@snowflake.com>
2025-07-14 18:38:05 +0200

    fix and add ref. test cases

--- structure/30_create_views.sql
+++ structure/30_create_views.sql
@@ -2,33 +2,38 @@
 -- 1. Basic View: Selects all data from RAW_IOT
 -- =========================================
 CREATE OR REPLACE VIEW ABC_IOT_ALL AS
-SELECT *
+SELECT
+   sensor_id AS id
+  ,SENSOR_0 AS type
+  ,SENSOR_1
+  ,SENSOR_2
+  ,SENSOR_3
 FROM RAW_IOT;
 
 -- =========================================
 -- 2. Filtered View: Only active sensors
 -- =========================================
-CREATE OR REPLACE VIEW ABC_IOT_ACTIVE AS
-SELECT *
+CREATE OR REPLACE VIEW ABC_IOT_ID_12 AS
+SELECT
+   sensor_id AS id
+  ,SENSOR_0 AS type
+  ,SENSOR_1
+  ,SENSOR_2
+  ,SENSOR_3
 FROM RAW_IOT
 WHERE SENSOR_ID = '12';
 
 -- =========================================
 -- 3. Column-Mapped View: Selected and renamed columns
 -- =========================================
-CREATE OR REPLACE VIEW ABC_IOT_SUMMARY AS
-SELECT
-    sensor_id AS id,
-    SENSOR_0 AS type,
-    SENSOR_1 AS timestamp,
-    SENSOR_2,
-    SENSOR_3
-FROM RAW_IOT;
-
+CREATE OR REPLACE VIEW ABC_IOT_ID_102_AVG AS
+SELECT AVG(SENSOR_0) as avg_102
+FROM RAW_IOT
+WHERE SENSOR_ID = 102;
 
 -- =========================================
 -- OPTIONAL CLEANUP SECTION (Teardown)
 -- =========================================
 --DROP VIEW IF EXISTS ABC_IOT_ALL;
---DROP VIEW IF EXISTS ABC_IOT_ACTIVE;
---DROP VIEW IF EXISTS ABC_IOT_SUMMARY;
+--DROP VIEW IF EXISTS ABC_IOT_ID_12;
+--DROP VIEW IF EXISTS ABC_IOT_ID_102_AVG;
