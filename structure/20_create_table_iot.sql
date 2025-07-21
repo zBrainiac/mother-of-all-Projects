0afe5a2739707783738607af3352b762dced4afc
sfc-gh-mdaeppen <marcel.daeppen@snowflake.com>
2025-07-21 13:46:25 +0200

    add  Set up github runner env

--- structure/20_create_table_iot.sql
+++ structure/20_create_table_iot.sql
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS AB_permanent_1_table_name_not_valide (
     created_at TIMESTAMP_TZ
 );
 
+USE ROLE ACCOUNTADMIN;
 
 CREATE TABLE IF NOT EXISTS MD_TEST.IOT_REF_20250711.ABC_permanent_not_valide_db_schema_prefix (
     id INT,
